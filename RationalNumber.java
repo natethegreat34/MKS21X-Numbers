8fd5020de4997aa3136ed9f9f16c1ceb12cb9102
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2018-10-15 21:49:17 -0400

    Classes and educated guessed methods

---
+++ RationalNumber.java
@@ -0,0 +1,36 @@
+public class RationalNumber{
+  public static void main(String[]args){
+  }
+  public RationalNumber(){}
+  private double q;
+  private int d;
+  private int n;
+
+  public double getQ () {
+    return q;
+}
+public int getN () {
+  return n;
+}
+public double getD () {
+  return d;
+}
+  public String toString (){
+    return "Hi."
+  }
+  public int setD (){
+    n = q * 10;
+    d = 10;
+  }
+  public int sameD (int a, int b, int c, int d){
+    a = a * d
+    b = b * d
+    c = c * b
+    d = d * b
+  }
+  public String FractForm (){
+    return "3" + "/" + "4";
+  }
+
+
+}
---
+++ RealNumber.java
@@ -0,0 +1,13 @@
+public class RealNumber{
+  public static void main(String[]args){
+  }
+public RealNumber(){}
+private double r;
+public getR () {
+  return r;
+}
+public String toString (){
+  return "Hello."
+}
+
+}
