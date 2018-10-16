61df52ab3cdce348403591cf65f2179f09a8de94
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2018-10-16 11:40:54 -0400

    Add files via upload

--- RealNumber.java
+++ RealNumber.java
@@ -1,13 +1,41 @@
 public class RealNumber{
-  public static void main(String[]args){
-  }
-public RealNumber(){}
-private double r;
-public getR () {
-  return r;
-}
-public String toString (){
-  return "Hello."
-}
+  private double value;
 
+  public RealNumber(double v){
+    value = v;
+  }
+
+  /*
+  *Return the sum of this and the other
+  */
+  public double add(RealNumber other){
+    return 0;
+  }
+  /*
+  *Return the product of this and the other
+  */
+  public double multiply(RealNumber other){
+    return 0;
+  }
+  /*
+  *Return the this divided by the other
+  */
+  public double divide(RealNumber other){
+    return 0;
+  }
+  /*
+  *Return the this minus the other
+  */
+  public double subtract(RealNumber other){
+    return 0;
+  }
+
+
+  public double getValue(){
+    return value;
+  }
+
+  public String toString(){
+    return ""+value;
+  }
 }
